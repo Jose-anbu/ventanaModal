4552f13355c0408ccef0782679d61ab2546fc571
José A. Burgos <jose_7892@hotmail.com>
2020-11-03 19:35:22 -0300

    Se agrega funcionalidad de ventana modal y su maquetado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import VentanaModal from './components/VentanaModal';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import VentanaModal from "./components/VentanaModal";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div>
+    <Container>
       <VentanaModal></VentanaModal>
-    </div>
+    </Container>
   );
 }
 
--- src/components/VentanaModal.js
+++ src/components/VentanaModal.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
 
 const VentanaModal = () => {
-    return (
-        <div>
-            ventana modal
-        </div>
-    );
+  const [show, setShow] = useState(true);
+  const handleClose = () => setShow(false);
+
+  return (
+    <Modal
+      show={show}
+      onHide={() => handleClose()}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton></Modal.Header>
+      <Modal.Body>
+        <img
+          src="https://lh3.googleusercontent.com/proxy/UvefFkDhKcIKpEALcaEpmXydNqzAJnI6YmY9CSVHNZ-1-gDmB-4NedzXjv5llxnPrOX0LNDsZVp5XXCBnig0bSWUPFcO7KTQODg5syfguEZCZFdqeGtgY9JZXy4RUhaEnCkEIrif_3ULqgbsuZTqUAnF"
+          alt="Línea de contención"
+          className="w-100"
+        ></img>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export default VentanaModal;
